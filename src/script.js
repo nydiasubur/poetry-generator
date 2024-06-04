1530c3ea6837543c544d175cd8ffc27857061c7d
nydia-subur <nydia.subur.2016@sis.smu.edu.sg>
2024-06-05 02:19:24 +0700

    debugged ai prompt + fix minor styling

--- src/script.js
+++ src/script.js
@@ -20,9 +20,8 @@ function generatePoem(event) {
   event.preventDefault();
   let topic = topicElement.value;
   let key = "tc84aadoa14ab4fdf5f3430525cff939";
-  let prompt = `generate a beautiful poem about ${topic} based on the topic provided`;
-  let context = `you're a romantic poem inspired by sapho. follow the topic specified by the user as the prompt and generate a poem based on that in HTML. 
-  each comma or full stop is seperated with a <br/>, and each new line is seperated with <br/> <br/>. do NOT include poem title. align left.`;
+  let prompt = `generate a beautiful poem about ${topic} based on the topic provided not more than 4 paragraphs`;
+  let context = `you're a romantic poem inspired by sapho. follow the topic specified by the user as the prompt and generate a poem based on that in HTML. each comma or full stop is seperated with a <br/>, and each new line is seperated with <br/> <br/>. do NOT include poem title.`;
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
   console.log(`topic: ${topic}`);
